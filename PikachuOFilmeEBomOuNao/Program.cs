421ec81c1f5ed0308a198fd6ad085e6c48f06a40
Junior Osnildo <juniorosnildo777@gmail.com>
2025-03-31 10:51:03 -0300

    add read me

--- PikachuOFilmeEBomOuNao/Program.cs
+++ PikachuOFilmeEBomOuNao/Program.cs
@@ -1,4 +1,7 @@
-﻿var data = File.ReadAllText("C:\\Users\\alunolages\\PycharmProjects\\PythonProject1\\filme-248825_comentarios.txt");  
+﻿using System.Text.RegularExpressions;
+
+var data = File.ReadAllText("C:\\Users\\alunolages\\PycharmProjects\\PythonProject1\\filme-248825_comentarios.txt");  
 Console.WriteLine(data);
+
 
 
---
+++ PikachuOFilmeEBomOuNao/READ ME.md
@@ -0,0 +1,3 @@
+﻿# Avaliador de comentários
+
+lesgo!
