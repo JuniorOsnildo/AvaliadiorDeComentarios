cda3750c7f23e1d6dd7215ac0057758b3f27191e
Junior Osnildo <juniorosnildo777@gmail.com>
2025-04-08 09:08:56 -0300

    0.0.2

--- PikachuOFilmeEBomOuNao/Program.cs
+++ PikachuOFilmeEBomOuNao/Program.cs
@@ -1,7 +1,35 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Text.RegularExpressions;
 
 var data = File.ReadAllText("C:\\Users\\alunolages\\PycharmProjects\\PythonProject1\\filme-248825_comentarios.txt");  
-Console.WriteLine(data);
+
+LinkedList<string> list = new LinkedList<string>();
+
+bool corte = false;
+
+foreach (var v in data)
+{
+    if (v == '\n') corte = true;
+    
+    if (corte)
+    {
+        if (v != '\n')
+            data.Split("\n").ToList();
+            
+        corte = false;
+    }
+    
+}
 
 
+Console.WriteLine(list.Count);
 
+
+int h = 0;
+
+foreach (var VARIABLE in list)
+{
+    Console.WriteLine(h);
+    Console.WriteLine(VARIABLE);
+    h++;
+}
