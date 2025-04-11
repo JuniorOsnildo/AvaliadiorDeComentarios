7b986d6b0614af3e7f7b07d859bcf8d5e8a13d43
isadorabsagaz <isadora.sagaz@gmail.com>
2025-04-11 15:03:41 -0300

    fix(Program): split do arquivo de comentarios com Regex, pois estava criando vetor com apenas 1 posição

--- PikachuOFilmeEBomOuNao/Program.cs
+++ PikachuOFilmeEBomOuNao/Program.cs
@@ -3,43 +3,13 @@ using System.Text.RegularExpressions;
 
 var data = File.ReadAllText("../../../Comentarios/filme-248825_comentarios.txt");
 
-string[] comentarios = data.Split(["\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
+string[] comentarios = Regex.Split(data, @"\r?\n\r?\n");
 
 for (int i = 0; i < comentarios.Length; i++)
 {
     Console.WriteLine(i);
     Console.WriteLine(comentarios[i]);
+    Console.WriteLine(new string('-', 100));
 }
 
-
-/*
-bool corte = false;
-
-foreach (var v in data)
-{
-    if (v == '\n') corte = true;
-
-    if (corte)
-    {
-        if (v != '\n')
-            data.Split("\n").ToList();
-
-        corte = false;
-    }
-
-}
-
-
-Console.WriteLine(list.Count);
-
-
-int h = 0;
-
-foreach (var variable in list)
-{
-    Console.WriteLine(h);
-    Console.WriteLine(variable);
-    h++;
-}
-
-*/
+Console.WriteLine(comentarios.Length);
