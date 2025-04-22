7b9679d8066c9805214589b1571f4ed16ffd6200
isadorabsagaz <isadora.sagaz@gmail.com>
2025-04-22 19:40:16 -0300

    feat: adiciona algumas palavras nos regex e testa-as

--- PikachuOFilmeEBomOuNao/Program.cs
+++ PikachuOFilmeEBomOuNao/Program.cs
@@ -11,11 +11,11 @@ List<Comentario> listaComentarios = new List<Comentario>();
 //gera lista de comentários e printa na tela
 for (int i = 0; i < comentarios.Length; i++)
 {
-    Console.WriteLine(i);
+    //Console.WriteLine(i);
     Comentario coment =  new Comentario(comentarios[i]);
     listaComentarios.Add(coment);
-    Console.WriteLine(listaComentarios[i].Coment);
-    Console.WriteLine(new string('-', 100));
+    //Console.WriteLine(listaComentarios[i].Coment);
+    //Console.WriteLine(new string('-', 100));
 }
 
 //printa numero de comentários
@@ -23,6 +23,7 @@ Console.WriteLine($"numero total de comentarios: {listaComentarios.Count}");
 
 //cria um avaliador dos comentários
 Avaliador avaliador = new Avaliador(listaComentarios); 
+
 avaliador.AvaliaComentario();
-avaliador.CategorizarComentarios();
-avaliador.VerificaCategoria();
+avaliador.CategorizaComentarios();
+avaliador.CalculaNotaFilme();
